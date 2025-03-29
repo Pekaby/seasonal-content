612f024220531ec883ed08518efda1443e4a5c49
Mikhail Antonov <26464626+Pekaby@users.noreply.github.com>
2025-03-29 12:40:45 +0300

    escaped generated html

--- assets/view/categories_managment.php
+++ assets/view/categories_managment.php
@@ -76,27 +76,60 @@ if(empty($categories)){
         ]
     ]);
 } else {
+    $categories_echo = '';
     foreach ($categories as $category) {
-        echo '<tr class="category">';
+        $categories_echo .= '<tr class="category">';
 
-        echo '<td>';
-        echo "<input type=\"text\" class=\"category_name\" value=\"".esc_html($category->title)."\" data-id=\"".esc_html($category->id)."\" placeholder=\"".esc_html__('Title', 'seasonal-content')."\">";
-        echo '</td>';
+        $categories_echo .= '<td>';
+        $categories_echo .= "<input type=\"text\" class=\"category_name\" value=\"".esc_html($category->title)."\" data-id=\"".esc_html($category->id)."\" placeholder=\"".esc_html__('Title', 'seasonal-content')."\">";
+        $categories_echo .= '</td>';
 
-        echo '<td>';
-        echo "<input type=\"date\" class=\"category_start_date\" value=\"".esc_html($category->date_start)."\" data-id=\"".esc_html($category->id)."\">";
-        echo '</td>';
+        $categories_echo .= '<td>';
+        $categories_echo .= "<input type=\"date\" class=\"category_start_date\" value=\"".esc_html($category->date_start)."\" data-id=\"".esc_html($category->id)."\">";
+        $categories_echo .= '</td>';
 
-        echo '<td>';
-        echo "<input type=\"date\" class=\"category_end_date\" value=\"".esc_html($category->date_end)."\" data-id=\"".esc_html($category->id)."\">";
-        echo '</td>';
+        $categories_echo .= '<td>';
+        $categories_echo .= "<input type=\"date\" class=\"category_end_date\" value=\"".esc_html($category->date_end)."\" data-id=\"".esc_html($category->id)."\">";
+        $categories_echo .= '</td>';
 
-        echo '<td>';
-        echo "<button class=\"category_delete\" data-id=\"".esc_html($category->id)."\">" . '
+        $categories_echo .= '<td>';
+        $categories_echo .= "<button class=\"category_delete\" data-id=\"".esc_html($category->id)."\">" . '
         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="undefined"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
         '."</button>";
-        echo '</td>';
+        $categories_echo .= '</td>';
     }
+    echo wp_kses($categories_echo, [
+        'tr' =>[
+            "class"           => true,
+        ],
+        'td' => [],
+        'input' => [
+            "id"              => true,
+            "type"            => true, 
+            "class"           => true,
+            "placeholder"     => true,
+            "value"           => true,
+        ],
+        'button' => [
+            "class"           => true,
+        ],
+        'svg'   => [
+            'class'           => true,
+            'aria-hidden'     => true,
+            'aria-labelledby' => true,
+            'role'            => true,
+            'xmlns'           => true,
+            'width'           => true,
+            'height'          => true,
+            'viewbox'         => true, 
+        ],
+        'g'     => [ 'fill'   => true ],
+        'title' => [ 'title'  => true ],
+        'path'  => [
+            'd'               => true, 
+            'fill'            => true,  
+        ]
+    ]);
 }
 ?>
         </tbody>
